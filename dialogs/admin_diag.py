87411a705135ae3261ec3e1da50346457c049407
bananzi <opio-12321@list.ru>
2025-04-22 15:00:07 +0300

    минор

--- dialogs/admin_diag.py
+++ dialogs/admin_diag.py
@@ -5,7 +5,7 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram import Bot
 from aiogram_dialog import Dialog, Window, DialogManager
 from aiogram_dialog.widgets.input import MessageInput
-from aiogram_dialog.widgets.kbd import Start
+from aiogram_dialog.widgets.kbd import Start, Button
 from aiogram_dialog.widgets.text import Const
 from aiogram.types import Message
 
@@ -20,11 +20,14 @@ bot = Bot(token=config.bot_token.get_secret_value())
 
 
 class AdminDialog(StatesGroup):
-    offline = State()
+    OFFLINE = State()
     START = State()
-    download = State()
+    DOWNLOAD = State()
+    ALL_SEND = State()
 
-
+async def wind_all_send(callback, button: Button,
+                   dialog_manager: DialogManager):
+    await dialog_manager.switch_to()
 async def download(message: Message,
                    message_input: MessageInput,
                    dialog_manager: DialogManager,):
@@ -36,13 +39,13 @@ admin_menu = Dialog(
     Window(
         Const(text='Здравствуйте админиcтратор, ваши полномочия: '),
         Start(Const("Загрузить все работы"),
-              id="download", state=AdminDialog.download),
-
+              id="download", state=AdminDialog.DOWNLOAD),
+        Button(Const("Отправить всем"), id="all_send", on_click=...),
         state=AdminDialog.START
     ),
     Window(
         Const(text="Напишите даты в формате «гггг-мм-дд»_«гггг-мм-дд»"),
         MessageInput(download),
-        state=AdminDialog.download
+        state=AdminDialog.DOWNLOAD
     )
 )
